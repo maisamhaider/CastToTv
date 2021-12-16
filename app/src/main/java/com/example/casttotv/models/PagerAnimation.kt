f9322205f6f705289b54a5481c7d077f58a5e4ef
maisam haider <maisamhaiderturi@gmail.com>
2021-12-16 18:26:04 +0500

    -> Tabs work done -> bookmarks work done (edit,delete,newtab in foreground, newtab in background, save as favorite) -> History work done (delete,newtab in foreground, newtab in background, save as favorite)

--- app/src/main/java/com/example/casttotv/models/PagerAnimation.kt
+++
@@ -1,5 +0,0 @@
-package com.example.casttotv.models
-
-import androidx.viewpager2.widget.ViewPager2
-
-data class PagerAnimation(val int: Int,val pageTransformer:ViewPager2.PageTransformer)
--- app/src/main/java/com/example/casttotv/utils/AppApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.casttotv.utils
-
-import android.app.Application
-import com.example.casttotv.database.AppDatabase
-
-class AppApplication : Application() {
-
-    val database: AppDatabase by lazy { AppDatabase.getInstance(this) }
-}
