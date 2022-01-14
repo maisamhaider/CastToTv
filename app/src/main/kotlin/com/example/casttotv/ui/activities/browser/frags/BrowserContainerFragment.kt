ff78f9ba269bc133abb2ee8d9527678ed225c134
maisam haider <maisamhaiderturi@gmail.com>
2022-01-14 17:50:38 +0500

    -> 100% browser ui done -> flow 75%

--- app/src/main/kotlin/com/example/casttotv/ui/activities/browser/frags/BrowserContainerFragment.kt
+++ app/src/main/kotlin/com/example/casttotv/ui/activities/browser/frags/BrowserContainerFragment.kt
@@ -27,6 +27,7 @@ class BrowserContainerFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentBrowserContainerBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
